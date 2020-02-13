0745f3648a22aa7b73b20e34755b76f2febf6923
lazyninjacat <matthew.riddett@gmail.com>
2020-02-13 13:11:56 -0800

    filename stuff

--- ConsoleQuizGame.js
+++ ConsoleQuizGame.js
@@ -20,80 +20,6 @@ c) correct answer (I would use a number for this)
 
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
-
-function Question(question, answers, correct) {
-  this.question = question;
-  this.answers = answers;
-  this.correct = correct;
-}
-
-var q1 = new Question(
-  "What has it got in its pockets?",
-  ["a: string", "b: nothing", "c: the precious"],
-  "c"
-);
-
-var q2 = new Question(
-  "What time is it Mr.Wolf?",
-  ["a: no idea", "b: LUNCH TIME!!!"],
-  "b"
-);
-
-var q3 = new Question(
-  "What is the square root of the hypotenuse if the third brother has green eyes?",
-  ["a: PIZZA!!", "b: 4.2 kerzics"],
-  "a"
-);
-
-var questions = [q1, q2, q3];
-
-var n = Math.floor(Math.random() * questions.length);
-
-var score = 0;
-
-NextQuestion();
-
-// Prompt the user for the answer, and allow them to exit
-function PromptUserAnswer() {
-  var submittedAnswer = prompt(
-    "Answer?.......... pst. type 'exit' to, y'know, cut and run.",
-    "enter answer here"
-  );
-
-  if (submittedAnswer === questions[n].correct) {
-    console.log("CORRECT!!! THE ANSWER IS: " + questions[n].correct);
-    score++;
-    console.log("Score increased to " + score + "!!!!!!");
-    NextQuestion();
-  } else if (submittedAnswer === "exit" || submittedAnswer === "Exit") {
-    console.log(
-      "Okay, we're done here. You got a total of " + score + ". Big whoop."
-    );
-  } else {
-    console.log("Nope! no points for you");
-    console.log("Score stays at " + score + ", boo-urns.");
-
-    NextQuestion();
-  }
-}
-
-// Display the current score in the console
-function ShowScore() {
-  console.log(score);
-}
-
-// Display the next Question
-function NextQuestion() {
-  n = Math.floor(Math.random() * questions.length);
-
-  console.log(questions[n].question);
-
-  for (i = 0; i < questions.length; i++) {
-    console.log(questions[n].answers[i]);
-  }
-
-  PromptUserAnswer();
-}
 /*
   --- Expert level ---
   
@@ -105,3 +31,83 @@ function NextQuestion() {
   
   11. Display the score in the console. Use yet another method for this.
   */
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// SOLUTION ATTEMPT #1
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+(function() {
+  function Question(question, answers, correct) {
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
+  }
+  var q1 = new Question(
+    "What has it got in its pockets?",
+    ["a: string", "b: nothing", "c: the precious"],
+    "c"
+  );
+  var q2 = new Question(
+    "What time is it Mr.Wolf?",
+    ["a: no idea", "b: LUNCH TIME!!!"],
+    "b"
+  );
+  var q3 = new Question(
+    "What is the square root of the hypotenuse if the third brother has green eyes?",
+    ["a: PIZZA!!", "b: 4.2 kerzics"],
+    "a"
+  );
+  var questions = [q1, q2, q3];
+  var n = Math.floor(Math.random() * questions.length);
+  var score = 0;
+  NextQuestion();
+
+  // Prompt the user for the answer, and allow them to exit
+  function PromptUserAnswer() {
+    var submittedAnswer = prompt(
+      "Answer?.......... pst. type 'exit' to, y'know, cut and run.",
+      "enter answer here"
+    );
+
+    if (submittedAnswer === questions[n].correct) {
+      console.log("CORRECT!!! THE ANSWER IS: " + questions[n].correct);
+      score++;
+      console.log("Score increased to " + score + "!!!!!!");
+      NextQuestion();
+    } else if (submittedAnswer === "exit" || submittedAnswer === "Exit") {
+      console.log(
+        "Okay, we're done here. You got a total of " + score + ". Big whoop."
+      );
+    } else {
+      console.log("Nope! no points for you");
+      console.log("Score stays at " + score + ", boo-urns.");
+      NextQuestion();
+    }
+  }
+
+  // Display the current score in the console
+  function ShowScore() {
+    console.log(score);
+  }
+
+  // Display the next Question
+  function NextQuestion() {
+    n = Math.floor(Math.random() * questions.length);
+    console.log(questions[n].question);
+    for (i = 0; i < questions.length; i++) {
+      console.log(questions[n].answers[i]);
+    }
+    PromptUserAnswer();
+  }
+})();
