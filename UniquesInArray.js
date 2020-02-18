8997493530fbc208ecce749d7b0f58d2707cdf39
lazyninjacat <matthew.riddett@gmail.com>
2020-02-18 15:42:47 -0800

    Find Uniques solution

---
+++ UniquesInArray.js
@@ -0,0 +1,47 @@
+/////////////////////////////////////////////////////////////////////////
+// Propkem Statement:
+
+// AlgoDaily Day 33
+
+// Given an array, return just the unique elements without using any
+// built-in Array filtering. In other words, you're removing any duplicates.
+
+// Note: Order needs to be preserved, so no sorting should be done.
+
+// function uniques(arr) {
+// fill in
+// }
+
+// let arr = [3, 5, 6, 9, 9, 4, 3, 12]
+// uniques(arr);
+// [3, 5, 6, 9, 4, 12]
+
+// arr = [13, 5, 3, 5, 8, 13, 14, 5, 9]
+// uniques(arr);
+// [13, 5, 3, 8, 14, 9]
+
+//////////////////////////////////////////////////////////////////////////
+// Solution:
+
+var startArray = [3, 5, 6, 9, 9, 4, 3, 12];
+var endArray = [];
+var isDuplicate = false;
+
+function findUniques() {
+  for (i = 0; i < startArray.length; i++) {
+    for (x = 0; x < i; x++) {
+      if (startArray[x] === startArray[i]) {
+        isDuplicate = true;
+      }
+    }
+    if (isDuplicate === false) {
+      endArray.push(startArray[i]);
+    }
+    isDuplicate = false;
+  }
+}
+
+findUniques();
+
+console.log("Start array = " + startArray);
+console.log("Uniques = " + endArray);
